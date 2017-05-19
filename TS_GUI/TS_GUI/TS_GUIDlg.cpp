9ec136f16b563f48ddb4b0b30db898f6f2469532
lucibr <lucian.bredean@outlook.com>
2017-05-19 19:37:07 +0300

    Text box cale folder

--- TS_GUI/TS_GUI/TS_GUIDlg.cpp
+++ TS_GUI/TS_GUI/TS_GUIDlg.cpp
@@ -123,6 +123,12 @@ BOOL CTS_GUIDlg::OnInitDialog()
 	aux_y = r.bottom - r.top;
 	aux_x = r.right - r.left;
 	elem->MoveWindow(0.01*screen_x_size, screen_y_size - (dim_y + 0.07*screen_y_size + 2*aux_y), aux_x, dim_y);
+	aux_x = r.right;
+
+	elem = GetDlgItem(IDC_CALE_IMG);
+	elem->GetWindowRect(&r);
+	dim_y = r.bottom - r.top;
+	elem->MoveWindow(aux_x + 0.01*screen_x_size, screen_y_size - (dim_y + 0.07*screen_y_size + 2 * aux_y), screen_x_size - (aux_x + 0.01*screen_x_size + screen_x_size*0.02), dim_y);
 
 	return TRUE;  // return TRUE  unless you set the focus to a control
 }
